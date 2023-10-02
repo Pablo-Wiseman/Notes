7c1bc988aa4cf3b26a06a98efa1c8ac73d29cf49
Pablo Wiseman <accessdevelopment@yandex.ru>
2023-10-02 22:03:47 +0500

    Adapter completed

--- app/src/main/java/com/example/notes/AddNoteActivity.java
+++ app/src/main/java/com/example/notes/AddNoteActivity.java
@@ -2,13 +2,59 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class AddNoteActivity extends AppCompatActivity {
 
+    private EditText editTextNote;
+    private RadioButton radioButtonLow;
+    private RadioButton radioButtonMedium;
+    private Button buttonSaveNote;
+    private NoteDataBase noteDataBase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+        noteDataBase = NoteDataBase.getInstance(getApplication());
+        initViews();
+
+        buttonSaveNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNote();
+            }
+        });
+    }
+
+    private void initViews(){
+        editTextNote = findViewById(R.id.editTextNote);
+        radioButtonLow = findViewById(R.id.radioButtonLow);
+        radioButtonMedium = findViewById(R.id.radioButtonMedium);
+        buttonSaveNote = findViewById(R.id.buttonSaveNote);
+    }
+
+    public static Intent newIntent(Context context){
+        return new Intent(context, AddNoteActivity.class);
+    }
+
+    private void saveNote(){
+        String text = editTextNote.getText().toString().trim();
+        int priority;
+        if(radioButtonLow.isChecked()){
+            priority = 0;
+        } else if(radioButtonMedium.isChecked()){
+            priority = 1;
+        } else {
+            priority = 2;
+        }
+        Note note = new Note(text, priority);
+        noteDataBase.notesDao().add(note);
+        finish();
     }
 }
--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.notes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerViewNotes;
+    private FloatingActionButton buttonAddNote;
+    private NotesAdapter notesAdapter;
+    private NoteDataBase noteDataBase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteDataBase = NoteDataBase.getInstance(getApplication());
+        initViews();
+
+        notesAdapter = new NotesAdapter();
+        recyclerViewNotes.setAdapter(notesAdapter);
+        notesAdapter.setOnNoteClickListener(new NotesAdapter.OnNoteClickListener() {
+            @Override
+            public void onNoteClick(Note note) {
+                noteDataBase.notesDao().remove(note.getId());
+                updateNotes();
+            }
+        });
+
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = AddNoteActivity.newIntent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        updateNotes();
+    }
+
+
+    private void initViews(){
+        recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
+        buttonAddNote = findViewById(R.id.buttonAddNote);
+    }
+    private void updateNotes(){
+        notesAdapter.setNotes(noteDataBase.notesDao().getNotes());
+
     }
 }
