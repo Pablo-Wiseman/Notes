cd92e9ab62d27e21502704727d83acb64ffab6eb
Pablo Wiseman <accessdevelopment@yandex.ru>
2023-10-03 09:37:06 +0500

    handler realized

--- app/src/main/java/com/example/notes/AddNoteActivity.java
+++ app/src/main/java/com/example/notes/AddNoteActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +19,7 @@ public class AddNoteActivity extends AppCompatActivity {
     private RadioButton radioButtonMedium;
     private Button buttonSaveNote;
     private NoteDataBase noteDataBase;
+    private Handler handler = new Handler(Looper.getMainLooper());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,7 +57,18 @@ public class AddNoteActivity extends AppCompatActivity {
             priority = 2;
         }
         Note note = new Note(text, priority);
-        noteDataBase.notesDao().add(note);
-        finish();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                noteDataBase.notesDao().add(note);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        finish();
+                    }
+                });
+            }
+        });
+        thread.start();
     }
 }
