77e45a6b2ee2442713cae1fd1ea5f32bac0c016d
Pablo-Wiseman <accessdevelopment@yandex.ru>
2023-10-04 19:42:12 +0500

    RxJava Disposable & CompositeDisposable

--- app/src/main/java/com/example/notes/MainViewModel.java
+++ app/src/main/java/com/example/notes/MainViewModel.java
@@ -8,24 +8,35 @@ import androidx.lifecycle.LiveData;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class MainViewModel extends AndroidViewModel {
 
     private NoteDataBase noteDataBase;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
     public MainViewModel(@NonNull Application application) {
         super(application);
         noteDataBase = NoteDataBase.getInstance(application);
     }
-    public void remove(Note note){
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                noteDataBase.notesDao().remove(note.getId());
-            }
-        });
-        thread.start();
+
+    public void remove(Note note) {
+
+       Disposable disposable =  noteDataBase.notesDao().remove(note.getId())
+                .subscribeOn(Schedulers.io())
+                .subscribe();
+
+       compositeDisposable.add(disposable);
+    }
+    @Override
+    protected void onCleared(){
+        super.onCleared();
+        compositeDisposable.dispose();
     }
 
-    public LiveData<List<Note>>getNotes(){
+    public LiveData<List<Note>> getNotes() {
         return noteDataBase.notesDao().getNotes();
     }
 }
