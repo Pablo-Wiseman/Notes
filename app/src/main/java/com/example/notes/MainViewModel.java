2c3f4e3ab44b7145d64056eeeaa58f0db7138e74
Pablo-Wiseman <accessdevelopment@yandex.ru>
2023-10-05 11:53:52 +0500

    single & completable realized

--- app/src/main/java/com/example/notes/MainViewModel.java
+++ app/src/main/java/com/example/notes/MainViewModel.java
@@ -8,8 +8,11 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Action;
@@ -32,7 +35,7 @@ public class MainViewModel extends AndroidViewModel {
     }
 
     public void updateList() {
-        Disposable disposable = noteDataBase.notesDao().getNotes()
+        Disposable disposable = getNotesRx()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Consumer<List<Note>>() {
@@ -44,9 +47,18 @@ public class MainViewModel extends AndroidViewModel {
         compositeDisposable.add(disposable);
     }
 
+    private Single<List<Note>>getNotesRx(){
+        return Single.fromCallable(new Callable<List<Note>>() {
+            @Override
+            public List<Note> call() throws Exception {
+                return noteDataBase.notesDao().getNotes();
+            }
+        });
+    }
+
     public void remove(Note note) {
 
-        Disposable disposable = noteDataBase.notesDao().remove(note.getId())
+        Disposable disposable = removeRx(note)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action() {
@@ -57,6 +69,15 @@ public class MainViewModel extends AndroidViewModel {
                 });
 
         compositeDisposable.add(disposable);
+    }
+
+    private Completable removeRx(Note note){
+        return Completable.fromAction(new Action() {
+            @Override
+            public void run() throws Throwable {
+                noteDataBase.notesDao().remove(note.getId());
+            }
+        });
     }
 
     @Override
